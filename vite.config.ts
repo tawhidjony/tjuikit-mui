648d46d52e2fc08e0db135b2e30eae0b4c1a7d16
TawhidJony <tawhid.jony.jhm@gmail.com>
2023-09-17 13:16:16 +0600

    alpha

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import react from '@vitejs/plugin-react-swc';
-import path from "path";
+import { glob } from 'glob';
+import { fileURLToPath } from 'node:url';
+import path, { extname, relative } from 'path';
 import { defineConfig } from 'vite';
 import dts from "vite-plugin-dts";
-
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
@@ -12,8 +14,23 @@ export default defineConfig({
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
+      
+      external: ["react", "react-dom", 'react/jsx-runtime'],
+      
+
+      input: Object.fromEntries(
+        glob.sync('**/*.{ts,tsx}').map(file => [
+          relative(
+            'dist',
+            file.slice(0, file.length - extname(file).length)
+          ),
+          fileURLToPath(new URL(file, import.meta.url))
+        ])
+      ),
+
       output: {
+        assetFileNames: 'assets/[name][extname]',
+        entryFileNames: '[name].js',
         globals: {
           react: "React",
           "react-dom": "ReactDOM",
@@ -23,5 +40,5 @@ export default defineConfig({
     sourcemap: true,
     emptyOutDir: true,
   },
-  plugins: [react(), dts()],
+  plugins: [react(), dts(),libInjectCss(),],
 })
